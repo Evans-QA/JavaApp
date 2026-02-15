54fc7964b5844c12033d3057d53257da565f43b7
Kevin <krawczynskikewin@gmail.com>
2026-02-15 17:57:30 +0100

    feat: add calculator with Scanner input and error handling

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double result = 0.0;
+        boolean validOperation = true;
 
-        int valueOne = 150;
-        int valueTwo = 280;
-        int maxValue = valueOne > valueTwo ? valueOne : valueTwo;
+        System.out.println("Hi user, welcome to the calculator");
+        System.out.println("Please, write first number");
+        double firstNumber = scanner.nextDouble();
 
-        // System.out.println(maxValue);
+        System.out.println("Please, write second number");
+        double secondNumber = scanner.nextDouble();
 
-        if (valueOne > valueTwo) {
-            System.out.println("That is correct");
+        System.out.println("Now please choose action (1-4):");
+        System.out.println("1. Add | 2. Subtract | 3. Multiply | 4. Divide");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            result = firstNumber + secondNumber;
+        } else if (choice == 2) {
+            result = firstNumber - secondNumber;
+        } else if (choice == 3) {
+            result = firstNumber * secondNumber;
+        } else if (choice == 4) {
+
+            if (secondNumber != 0) {
+                result = firstNumber / secondNumber;
+            } else {
+                System.out.println("Error: Cannot divide by zero.");
+                validOperation = false;
+            }
+        } else {
+            System.out.println("Error: Invalid menu choice.");
+            validOperation = false;
         }
-        else {
-            System.out.println("That is false");
+
+        if (validOperation) {
+            System.out.println("The result is: " + result);
         }
 
+        scanner.close();
     }
 }
