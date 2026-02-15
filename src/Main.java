e038e296aef048905c9e4a3ec28e99a7eb7973aa
Kevin <krawczynskikewin@gmail.com>
2026-02-15 17:24:53 +0100

    feat: implement conditional logic using if-else and ternary operators

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
- int valueA = 1;
- int valueB = 2;
- int valueC = 3;
- int valueD = 4;
+        int valueOne = 150;
+        int valueTwo = 280;
+        int maxValue = valueOne > valueTwo ? valueOne : valueTwo;
 
-  int result1 = valueD / valueB + valueA;
-  int result2 = valueC * valueB * 2 / valueD + valueA;
+        // System.out.println(maxValue);
 
- // System.out.println(result1);
- // System.out.println(result2);
-
- int i1 = 75;
- var i2 = 25;
-
- // System.out.println(i1 + i2);
-
-        var myName = "Kevin";
-        var myAge = 25;
-        var myWeight = 91.5;
-
-        // System.out.println("Hi " + myName + " you are " + myAge + " years old and you weight " + myWeight);
-
-        int finalInt = 5;
-
-        System.out.println(finalInt);
-
-        finalInt = 10;
-
-        System.out.println(finalInt);
-
+        if (valueOne > valueTwo) {
+            System.out.println("That is correct");
+        }
+        else {
+            System.out.println("That is false");
+        }
 
     }
 }
